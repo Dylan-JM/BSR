c7df8aac37226586ca7d24c3ab4c5c55dbbbcbf9
Dylan-JM <dylanjmarley01@gmail.com>
2026-03-30 16:58:20 +0100

    initial commit

---
+++ BSR/Models/Home.cs
@@ -0,0 +1,10 @@
+﻿namespace BSR.Models;
+
+public class Home
+{
+    public int Id { get; set; }
+    public string Address { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public int Area { get; set; }
+    public string ImageURL { get; set; } = string.Empty;
+}
