aeb368672a7aa70d71e723c2fd4768d25e3d73e9
Dylan-JM <dylanjmarley01@gmail.com>
2026-04-02 20:50:54 +0100

    feat: View details page

---
+++ BSR/Pages/HomeDetail.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using BSR.Models;
+using BSR.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BSR.Pages;
+
+public class HomeDetail : PageModel
+{
+    private readonly HomeService _homeService;
+
+    public HomeDetail(HomeService homeService)
+    {
+        _homeService = homeService;
+    }
+
+    public Home Home { get; set; }
+
+    public IActionResult OnGet(int id)
+    {
+        Home = GetHomeById(id);
+        return Page();
+    }
+
+    private Home GetHomeById(int id)
+    {
+        return _homeService.GetHomeById(id);
+    }
+}
