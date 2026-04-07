a24ec88a33058ee8f5afef1620bbcf63cd726397
Dylan-JM <dylanjmarley01@gmail.com>
2026-04-07 19:48:57 +0100

    using database

--- BSR/Program.cs
+++ BSR/Program.cs
@@ -1,16 +1,27 @@
+using BSR.Models;
 using BSR.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddSingleton<HomeService>();
 
 builder.Services.AddRazorPages(opt =>
 {
     opt.Conventions.ConfigureFilter(new IgnoreAntiforgeryTokenAttribute());
 });
 
+builder.Services.AddScoped<HomeService>();
+
+builder.Services.AddDbContext<HomeContext>(opt => opt.UseSqlite("Data Source=bsr.db"));
+
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<HomeContext>();
+
+    dbContext.Database.EnsureDeleted();
+    dbContext.Database.EnsureCreated();
+}
 
 app.UseStaticFiles();
 
